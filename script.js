4c62bd81f0d0fcef4eeeecdb2330a1814aa048dd
Frikilinux <frikilinux@gmail.com>
2024-01-23 03:09:35 -0300

    Change functions

--- script.js
+++ script.js
@@ -6,41 +6,55 @@ const keys = [
   ['u', 'ufat'],
 ]
 
+const reverseKeys = () => {
+  let newKeys = []
+  for (let i = 0; i < keys.length; i++) {
+    newKeys[i] = keys[i].reverse()
+  }
+  return newKeys
+}
+
 const setOutputText = (output) => {
   document.querySelector('.encrypted-empty').style.display = 'none'
   document.querySelector('.output-message').style.display = 'flex'
   document.querySelector('.output-message p').textContent = output
 }
 
-const encryptMessage = () => {
+const processMsg = (action) => {
+  let newKeys = keys
+
+  if (action === 'decrypt') {
+    newKeys = reverseKeys()
+  }
+
   const messageInput = document.querySelector('.message-input').value
   if (!messageInput) return
 
   let encryptedText = messageInput.toLowerCase()
 
-  for (let i = 0; i < keys.length; i++) {
-    if (encryptedText.includes(keys[i][0])) {
-      encryptedText = encryptedText.replaceAll(keys[i][0], keys[i][1])
+  for (let i = 0; i < newKeys.length; i++) {
+    if (encryptedText.includes(newKeys[i][0])) {
+      encryptedText = encryptedText.replaceAll(newKeys[i][0], keys[i][1])
     }
   }
 
   setOutputText(encryptedText)
 }
 
-const decryptMessage = () => {
-  const messageInput = document.querySelector('.message-input').value
-  if (!messageInput) return
+// const decryptMessage = () => {
+//   const messageInput = document.querySelector('.message-input').value
+//   if (!messageInput) return
 
-  let encryptedText = messageInput.toLowerCase()
+//   let encryptedText = messageInput.toLowerCase()
 
-  for (let i = 0; i < keys.length; i++) {
-    if (encryptedText.includes(keys[i][1])) {
-      encryptedText = encryptedText.replaceAll(keys[i][1], keys[i][0])
-    }
-  }
+//   for (let i = 0; i < keys.length; i++) {
+//     if (encryptedText.includes(keys[i][1])) {
+//       encryptedText = encryptedText.replaceAll(keys[i][1], keys[i][0])
+//     }
+//   }
 
-  setOutputText(encryptedText)
-}
+//   setOutputText(encryptedText)
+// }
 
 const copyMessage = () => {
   const encryptedText = document.querySelector('.output-message p').textContent
